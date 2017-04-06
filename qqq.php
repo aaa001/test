19cebd2dda3eebb56885c3bde4a64c2ca24a6f92
Ярослав <yaroslav.te@hotmail.com>
2017-04-06 11:36:34 +0300

    Version 1.0

---
+++ qqq.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Returns comment
+ * @param int $x
+ * some comment
+ * @param string $y
+ * some other comment
+ * @return string
+ */
+function xxx($x, $y)
+{
+
+}
---
+++ zzz.php
@@ -0,0 +1,16 @@
+<?php
+
+
+/**
+ * blah blah
+ * @param intagar $x
+ * one param
+ * @param $y
+ * second param
+ * @return something
+ * blah blah
+ */
+function zzzz($x, $y)
+{
+    phpversion();
+}
